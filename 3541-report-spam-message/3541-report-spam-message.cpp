552be0cba64937ff8fb0316d01b2a352660fb7f5
IQRAAMIR1 <iqraamir961@gmail.com>
2025-03-30 11:23:46 +0500

    Time: 163 ms (99.27%), Space: 288.5 MB (90.51%) - LeetHub

---
+++ 3541-report-spam-message/3541-report-spam-message.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool reportSpam(vector<string>& message, vector<string>& bannedWords) {
+        unordered_set<string> bannedSet(bannedWords.begin(), bannedWords.end());
+        int count = 0;
+
+        for (string& word : message) {
+            if (bannedSet.count(word)) {
+                count++;
+                if (count >= 2) return true; 
+            }
+        }
+        return false;
+    }
+};
+
