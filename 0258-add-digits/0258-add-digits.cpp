2ec2c2a9a4523491a01264e810a7cb900a867dc8
IQRAAMIR1 <iqraamir961@gmail.com>
2025-04-06 23:03:53 +0500

    Time: 0 ms (100%), Space: 8.5 MB (97.98%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    int addDigits(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        return 1 + (num - 1) % 9;
+    }
+};
